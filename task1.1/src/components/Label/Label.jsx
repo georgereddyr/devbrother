6de5841d554d06b4dd6f783221a27a5e92a6b07c
georgereddyr <71940511+georgereddyr@users.noreply.github.com>
2022-02-14 13:30:41 +0300

    Delete task1.1 directory

--- task1.1/src/components/Label/Label.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../App.css';
-
-export default class Label extends React.Component { 
-  render() {
-    return <label htmlFor="randomName">{this.props.children}</label>
-  }
-}
